b06ddb19a4b5b88e0d62fc2e0091ed99d4466f9f
Aleksandar <aleksaks27@gmail.com>
2024-04-01 18:15:04 +0100

    Make the form responsive for different screen sizes and improve the page styling

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const form = document.querySelector("#form");
+const firstName = document.querySelector("#first_name");
+const submitButton = document.querySelector("#submit-button");
 let password = document.querySelector("#pwd");
 let confirmPassword = document.querySelector("#confirm_pwd");
 let passwordMessage = document.querySelector("#pwd_message");
@@ -25,5 +28,12 @@ function checkPassword() {
         }
     })
 }
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if ((first) && (first === second)) {
+        alert(`Hey ${firstName.value}, thanks for signing up. Check your inbox soon for our email.`);
+    }
+})
 
 checkPassword();
